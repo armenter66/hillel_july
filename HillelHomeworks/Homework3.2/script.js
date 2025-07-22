ea198b86bd1e1b8e8135b7c4e2f75ae1ccdd99a9
armenter66 <armenter66@gmail.com>
2025-07-22 12:09:34 +0300

    Homework3.2

---
+++ HillelHomeworks/Homework3.2/script.js
@@ -0,0 +1,9 @@
+let yourName = prompt('Please write your name here');
+let yourNickName = prompt('Please write your nickname here');
+let yourAge = prompt('Please write your age here');
+
+alert(`Your name is ${yourName}, your nickname is ${yourNickName}, your age is 
+    ${yourAge}`);
+
+console.log(`Your name is ${yourName}, your nickname is ${yourNickName}, your age is 
+    ${yourAge}`);
