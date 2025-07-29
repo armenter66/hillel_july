62c61b00d9c688a1bdf3fd584f893d58486f0b2e
armenter66 <armenter66@gmail.com>
2025-07-29 15:08:14 +0300

    HomeworkAndPractice

---
+++ HillelHomeworks/Homework5/script.js
@@ -0,0 +1,90 @@
+// Task 1
+
+let numbersList = '';
+
+for (let i = 20; i <= 30; i += 0.5) {
+	numbersList += i + ', ';
+}
+
+console.log(numbersList);
+
+// Task 2
+
+let dollar = 42;
+
+let dollarList = '';
+
+for (let i = 10; i <= 100; i += 10) {
+	dollarList += i * dollar + ', ';
+}
+
+console.log(dollarList);
+
+// Task 3
+
+// let integerNum = parseInt(prompt('Enter a integer value'));
+// let integerList = '';
+
+// for (let i = 1; i <= 100; i++) {
+// 	if (i ** 2 <= integerNum) {
+// 		integerList += i + ', ';
+// 	}
+// }
+
+// console.log(integerList);
+
+// Task 4
+
+// let integerNum2 = parseInt(prompt('Enter a integer value!'));
+
+// let isInteger = true;
+
+// if (integerNum2 <= 1) {
+// 	isInteger = false;
+// } else {
+// 	for (let i = 2; i < integerNum2; i++) {
+// 		if (integerNum2 % i === 0) {
+// 			isInteger = false;
+// 			break;
+// 		}
+// 	}
+// }
+
+// if (isInteger) {
+// 	console.log(integerNum2 + ' is a integer number');
+// } else {
+// 	console.log(integerNum2 + ' isnt a integer number');
+// }
+
+// Task 5
+
+let numberToCheck = parseInt(
+	prompt('Enter an integer value to check a power of 3')
+);
+
+let powerValue = 1;
+let isPower = false;
+
+while (powerValue <= numberToCheck) {
+	if (powerValue === numberToCheck) {
+		isPower = true;
+		break;
+	}
+	powerValue *= 3;
+}
+
+// for (let i = 1; i <= numberToCheck; i++) {
+// 	if (i === numberToCheck) {
+// 		isPower = true;
+// 		break;
+// 	} else {
+// 		i *= 3;
+// 		console.log(i, numberToCheck);
+// 	}
+// }
+
+if (isPower) {
+	console.log(numberToCheck + ' can be obtained as a power of 3');
+} else {
+	console.log(numberToCheck + ' cant be obtained as a power of 3');
+}
---
+++ Lessons/Lesson5/app.js
@@ -0,0 +1,128 @@
+// Task 1
+let numbersFromTenToTwenty = ' ';
+
+for (let i = 10; i <= 20; i++) {
+	numbersFromTenToTwenty += i;
+	if (i < 20) {
+		numbersFromTenToTwenty += ', ';
+	}
+}
+
+console.log(numbersFromTenToTwenty);
+
+// Task 2
+
+let numbersSquared = '';
+
+for (let i = 10; i <= 20; i++) {
+	numbersSquared += i ** 2;
+	if (i < 20) {
+		numbersSquared += ', ';
+	}
+}
+console.log(numbersSquared);
+
+// Task 3
+
+let tableOfMultiply = '';
+
+for (let i = 1; i <= 10; i++) {
+	tableOfMultiply += `${i} * 7 = ${i * 7}`;
+	if (i < 10) {
+		tableOfMultiply += '; ';
+	}
+}
+
+console.log(tableOfMultiply);
+
+// Task 4
+
+let sumFromOneToFive = 0;
+
+for (let i = 1; i <= 15; i++) {
+	sumFromOneToFive += i;
+}
+
+console.log(sumFromOneToFive);
+
+// Task 5
+
+let productOfNumbers = 1;
+
+for (let i = 15; i < 35; i++) {
+	productOfNumbers *= i;
+}
+
+console.log(productOfNumbers);
+3.386540329888963e27;
+3.386540329888963e27;
+
+// Task 6
+
+let sumOfNumbers = 0;
+
+for (let i = 1; i <= 500; i++) {
+	sumOfNumbers += i;
+}
+
+let average = sumOfNumbers / 500;
+
+console.log(average);
+
+// Task 7
+
+let sumOfEvenNumb = 0;
+
+for (let i = 30; i <= 80; i++) {
+	if (i % 2 === 0) {
+		sumOfEvenNumb += i;
+	}
+}
+console.log(sumOfEvenNumb);
+
+// Task 8
+
+let numbMultipleByThree = '';
+
+for (let i = 100; i <= 200; i++) {
+	if (i % 3 === 0) {
+		numbMultipleByThree += i + ', ';
+	}
+}
+
+console.log(numbMultipleByThree);
+
+// Task 9
+
+let naturalNumb = parseInt(prompt('Enter a natural number'));
+let numbsDivisor = '';
+let evenCount = 0;
+let evenSum = 0;
+
+for (let i = 1; i <= naturalNumb; i++) {
+	if (naturalNumb % i === 0) {
+		numbsDivisor += i + ', ';
+
+		if (i % 2 === 0) {
+			evenCount++;
+			evenSum += i;
+		}
+	}
+}
+
+console.log(numbsDivisor);
+console.log(evenCount);
+console.log(evenSum);
+
+// Task 10
+
+let tableOfMultiple = '';
+
+for (let i = 1; i <= 10; i++) {
+	for (let j = 1; j <= 10; j++) {
+		tableOfMultiple += `${i} * ${j} = ${i * j}; \n`;
+	}
+	tableOfMultiple += '\n';
+}
+
+console.log(tableOfMultiple);
