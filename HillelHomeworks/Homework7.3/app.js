79d7cfdc162a17b783dbb5e4ff31b3af5c6261e1
armenter66 <armenter66@gmail.com>
2025-08-12 14:55:45 +0300

    homework7.3

--- HillelHomeworks/Homework7.3/app.js
+++ HillelHomeworks/Homework7.3/app.js
@@ -1,15 +1,13 @@
 function writeNumbers() {
-	let lastNumber = 0;
-	let numbersArray = [];
+	let lastNumber;
 
-	for (let i = 0; i < 3; i++) {
-		let numb = parseInt(prompt('Write number  bigger then 100'));
+	for (let i = 0; i < 10; i++) {
+		let numb = parseInt(prompt('Write number bigger then 100'));
 		if (isNaN(numb) || numb <= 100) {
 			alert('Only numbers bigger then 100!!!');
-			i--;
 		} else {
-			numbersArray.push(numb);
-			lastNumber = numbersArray[numbersArray.length - 1];
+			lastNumber = numb;
+			break;
 		}
 	}
 
