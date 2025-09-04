69500cf1dd9bcd5e701d33b58bde2ba6e882c50f
armenter66 <armenter66@gmail.com>
2025-09-04 16:56:12 +0300

    Lesson12-Homework

--- Lessons/Lesson12/js/app.js
+++ Lessons/Lesson12/js/app.js
@@ -1,78 +1,93 @@
 const showCategories = () => {
-  const parent = document.querySelector('.categories');
-  if (!parent) {
-    return;
-  }
+	const parent = document.querySelector('.categories');
+	if (!parent) {
+		return;
+	}
 
-  const categoriesList = document.createElement('ul');
-  categoriesList.addEventListener('click', event => {
-    if (event.target && event.target.tagName === 'LI') {
-      const categoryId = event.target.getAttribute('data-category');
-      // const category = getCategoryById(categoryId);
-      const category = categories[categoryId];
-      if (!category) {
-        return;
-      }
-      // console.log(category);
-      showProductsByCategory(category);
+	const categoriesList = document.createElement('ul');
+	categoriesList.addEventListener('click', (event) => {
+		if (event.target && event.target.tagName === 'LI') {
+			const categoryId = event.target.getAttribute('data-category');
+			// const category = getCategoryById(categoryId);
+			const category = categories[categoryId];
+			if (!category) {
+				return;
+			}
+			// console.log(category);
+			showProductsByCategory(category);
+		}
+	});
 
-    }
-  });
+	Object.values(categories).forEach((category) => {
+		const element = document.createElement('li');
+		element.textContent = category.name;
+		element.setAttribute('data-category', category.id);
 
-  Object.values(categories).forEach(category => {
-    const element = document.createElement('li');
-    element.textContent = category.name;
-    element.setAttribute('data-category', category.id);
+		// element.addEventListener('click', () => {
+		//   console.log(category);
+		// });
 
-    // element.addEventListener('click', () => {
-    //   console.log(category);
-    // });
+		categoriesList.appendChild(element);
+	});
 
-    categoriesList.appendChild(element);
-  });
-
-  parent.appendChild(categoriesList);
-}
+	parent.appendChild(categoriesList);
+};
 
 // const getCategoryById = id => categories.find(category => category.id == id);
 
-const showProductsByCategory = category => {
-  // const { products } = category; те саме, що і нижче
-  const products = category.products;
-  const parent = document.querySelector('.products');
-  if (!parent) {
-    return;
-  }
+const showProductsByCategory = (category) => {
+	// const { products } = category; те саме, що і нижче
+	const products = category.products;
+	const parent = document.querySelector('.products');
+	if (!parent) {
+		return;
+	}
 
-  parent.innerHTML = '';
+	parent.innerHTML = '';
 
-  const productsList = document.createElement('ul');
+	const productsList = document.createElement('ul');
 
-  productsList.addEventListener('click', event => {
-    if (event.target && event.target.tagName === 'LI') {
-      console.log(event.target);
-      // const categoryId = category.id
-      const categoryId = event.target.getAttribute('data-category');
-      const productId = event.target.getAttribute('data-product');
-      // TODO: (at home)
-      // Add content to the third column
-    }
-  });
+	productsList.addEventListener('click', (event) => {
+		if (event.target && event.target.tagName === 'LI') {
+			console.log(event.target);
+			// const categoryId = category.id
+			const categoryId = event.target.getAttribute('data-category');
+			const productId = event.target.getAttribute('data-product');
+			// TODO: (at home)
+			// Add content to the third column
+			const category = categories[categoryId];
+			if (!category) return;
 
+			const product = category.products.find((e) => e.id == productId);
+			if (!product) return;
 
-  products.forEach(product => {
-    const element = document.createElement('li');
-    element.textContent = `${product.name} - $${product.price}`;
-    element.setAttribute('data-product', product.id);
-    element.setAttribute('data-category', category.id);
-    // element.addEventListener('click', () => {
-    //   console.log(product);
-    // })
-    productsList.appendChild(element);
-  });
+			showProductInfo(product);
+		}
+	});
 
-  parent.appendChild(productsList);
-}
+	products.forEach((product) => {
+		const element = document.createElement('li');
+		element.textContent = `${product.name} - $${product.price}`;
+		element.setAttribute('data-product', product.id);
+		element.setAttribute('data-category', category.id);
+		// element.addEventListener('click', () => {
+		//   console.log(product);
+		// })
+		productsList.appendChild(element);
+	});
 
+	parent.appendChild(productsList);
+};
+
+const showProductInfo = (product) => {
+	const parent = document.querySelector('.info');
+	if (!parent) return;
+
+	parent.innerHTML = `
+    <h3>${product.name}</h3>
+    <p>${product.description}</p>
+    <span>Price: $${product.price}</span>
+  `;
+};
 
 showCategories();
