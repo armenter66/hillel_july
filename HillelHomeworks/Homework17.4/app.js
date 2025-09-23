17356b0e793abada51577cf92bb713f14bce23e1
armenter66 <armenter66@gmail.com>
2025-09-23 18:08:08 +0300

    homework17.4

--- HillelHomeworks/Homework17.4/app.js
+++ HillelHomeworks/Homework17.4/app.js
@@ -1,7 +1,71 @@
-// Створити і описати сутності Багатоквартирного будинку, квартири, мешканців
-// Додати можливість створювати нові будинки на певну кількість квартир з певною кількістю мешканців
-// Кількість квартир в будинку та мешканців в кожній квартирі задає користувач
-// Реалізувати перевірку на пусті значення
-// Квартири - це Map всередині будинку, а Мешканці - це Set в квартирі
-// Реалізувати функціонал виводу даних по будинку після створення
-// Функціонал, що пов'язаний з розробкою UI є опціональним, проте загальна логіка взаємодії класів - обов'язкова до виконання,
+class House {
+	apartments = new Map();
+
+	addApartment(obj) {
+		this.apartments.set(obj.number, obj);
+	}
+}
+
+House.prototype.createHouse = function () {
+	this.apartments.forEach((apartment, number) => {
+		console.log(`Apartment ${number}: ${apartment.roomsAmount} rooms`);
+		if (apartment.people.size > 0) {
+			console.log('Peoples:');
+			apartment.people.forEach((person) => {
+				console.log(person.name);
+			});
+		} else {
+			console.log(' No peoples in apartment');
+		}
+	});
+};
+class Apartment {
+	constructor(number, roomsAmount, people = []) {
+		this.number = number;
+		this.roomsAmount = roomsAmount;
+		this.people = new Set(people);
+	}
+
+	addPerson(person) {
+		if (person && person.name) {
+			this.people.add(person);
+		} else {
+			console.log('Name doesnt exist');
+		}
+	}
+}
+class Person {
+	constructor(name) {
+		this.name = name;
+	}
+}
+
+const people = [
+	new Person('Oleg'),
+	new Person('Alex'),
+	new Person('Olga'),
+	new Person('Victor'),
+	new Person('Maks'),
+	new Person('Olena'),
+	new Person('Igor'),
+];
+
+const apartments = [
+	new Apartment(102, 3, people),
+	new Apartment(103, 1, people),
+	new Apartment(105, 2, people),
+	new Apartment(110, 4, people),
+	new Apartment(122, 3, people),
+	new Apartment(104, 4, people),
+	new Apartment(131, 2, people),
+];
+
+const bigHouse = new House();
+
+apartments.forEach((app) => {
+	bigHouse.addApartment(app);
+});
+
+console.log(bigHouse);
+
+bigHouse.createHouse();
