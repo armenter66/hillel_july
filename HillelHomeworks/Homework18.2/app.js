036af58e1f633a4259ac4b81717231abc4c38c49
armenter66 <armenter66@gmail.com>
2025-09-28 17:49:20 +0300

    homework18.2

--- HillelHomeworks/Homework18.2/app.js
+++ HillelHomeworks/Homework18.2/app.js
@@ -1,9 +1,75 @@
-// Додаткове (обов'язкове) домашнє завдання:
+const prevBtn = document.querySelector('.slider__prev');
+const nextBtn = document.querySelector('.slider__next');
+const slides = Array.from(document.querySelectorAll('.slider__item'));
+const dotsWrapper = document.querySelector('.dots');
 
-// Реалізувати слайдер
-// Змінювати кожні 3 секунди зображення (масив з 5+ зображень)
-// Реалізувати переключення по стрілках (вперед/назад). 
-// Слайдер має працювати циклічно: при досягненні останнього зображення і при кліці на стрілку “вперед” - показувати перше зображення
-// Слайдер пишемо самі, варіанти з інтернету - не використовуємо
-// Анімація та інші ефекти - за бажанням
-// Вивчити теорію по Event Loop та Promise
+slides.forEach((slide, index) => {
+	const dot = document.createElement('a');
+	if (index === 0) {
+		dot.classList.add('active');
+	}
+	dot.addEventListener('click', () => showSlide(index));
+	dotsWrapper.appendChild(dot);
+});
+
+const dots = document.querySelectorAll('.dots a');
+
+let currentIndex = 0;
+
+function showSlide(index) {
+	slides[currentIndex].classList.remove('active');
+	dots[currentIndex].classList.remove('active');
+
+	slides[index].classList.add('active');
+	dots[index].classList.add('active');
+
+	currentIndex = index;
+}
+
+function nextSlide() {
+	let nextIndex = currentIndex + 1;
+	if (nextIndex >= slides.length) {
+		nextIndex = 0;
+	}
+
+	showSlide(nextIndex);
+}
+
+function prevSlide() {
+	let prevIndex = currentIndex - 1;
+	if (prevIndex < 0) {
+		prevIndex = slides.length - 1;
+	}
+
+	showSlide(prevIndex);
+}
+
+const loopTime = 3000;
+let intervalID = setInterval(nextSlide, loopTime);
+
+function resetInterval() {
+	clearInterval(intervalID);
+	intervalID = setInterval(nextSlide, loopTime);
+}
+
+nextBtn.addEventListener('click', () => {
+	nextSlide();
+	resetInterval();
+});
+
+prevBtn.addEventListener('click', () => {
+	prevSlide();
+	resetInterval();
+});
+
+showSlide(currentIndex);
+
+// const intervalID = setInterval(nextSlide, 500);
+
+// nextBtn.addEventListener('click', () => {
+// 	nextSlide();
+// 	clearInterval(intervalID);
+// 	intervalID = setInterval(nextSlide, 500);
+// });
+
+// prevBtn.addEventListener('click', prevSlide);
